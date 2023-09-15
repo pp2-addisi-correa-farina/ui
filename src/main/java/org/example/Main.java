eb42ea835df356209c90bf83edc1db4dad606052
Angelo <angelofaria060@gmail.com>
2023-09-15 16:53:10 -0300

    agregue libs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,14 @@ import org.pp2.ClimaTotal;
 import org.pp2.Dispositivo;
 
 import java.io.FileNotFoundException;
+import java.nio.file.FileSystems;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        List<Dispositivo> dispositivos = ClimaTotal.inicializarDispositivos("src/main/resources/dispositivos.json", "libs/");
+        List<Dispositivo> dispositivos = ClimaTotal.inicializarDispositivos(
+                FileSystems.getDefault().getPath("src", "main", "resources", "dispositivos.json").toString(),
+                FileSystems.getDefault().getPath("libs").toString());
         View view = new View(dispositivos);
         view.init();
     }
